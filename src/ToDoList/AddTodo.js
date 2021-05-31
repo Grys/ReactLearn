9c1346ca2711872bfa87c476bdb1f8dae7e9bd3e
Ryslan <giljazov@gmail.com>
2021-05-31 13:27:09 +0300

    Custom hook

--- src/ToDoList/AddTodo.js
+++ src/ToDoList/AddTodo.js
@@ -2,20 +2,32 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import './AddTodo.css'
 
+function useInputValue(defaultValue='') {
+    const [value, setValue] = useState(defaultValue)
+    return {
+        bind : {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
 function AddTodo( {onCreate}) 
 {
-    const [value, setValue] = useState('')
+    const input = useInputValue('')
 
     function handleSubmit(event) {
-        event.preventDefault();
-        if (value.trim())
-            onCreate(value)
-        setValue('')
+        event.preventDefault();        
+        if (input.value().trim())
+            onCreate(input.value())
+        input.clear()
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input value={value} onChange={event => setValue(event.target.value)} />
+            <input {...input.bind} />
             <button type='submit'>Add Todo</button>
         </form>
     )
